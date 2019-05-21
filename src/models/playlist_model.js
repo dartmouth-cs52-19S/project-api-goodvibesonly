38766203112033847c6a03e7c96d932b6035cb79
Grace <Grace.M.Dorgan.21@dartmouth.edu>
2019-05-21 18:28:10 -0400

    solidifying playlist model

--- src/models/playlist_model.js
+++ src/models/playlist_model.js
@@ -6,9 +6,9 @@ const PlaylistSchema = new Schema({
   title: { type: String, unique: true, lowercase: true },
   author: { type: String },
   songs: [{
-    name: String,
-    artist: String,
+    songid: String,
   }],
+  id: '',
 },
 {
   toJSON: {
