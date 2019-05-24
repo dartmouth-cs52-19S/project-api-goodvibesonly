0c37567210f80b042bef8fb84c50d6f72d0d2838
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-05-24 16:10:59 -0400

    print statements

--- src/controllers/player_controller.js
+++ src/controllers/player_controller.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const API_PLAYER_URL = 'https://api.spotify.com/v1/me/player';
 
 export const getPlayState = (req, res) => {
-  axios.get(`${API_PLAYER_URL}/currently-playing`, { headers: { authorization: `Bearer ${req.token}` } })
+  console.log(req.body.token);
+  axios.get(`${API_PLAYER_URL}/currently-playing`, { headers: { authorization: `Bearer ${req.body.token}` } })
     .then((response) => {
       res.send(response.data);
       console.log(JSON.stringify(response.data));
@@ -14,7 +15,7 @@ export const getPlayState = (req, res) => {
 };
 
 export const sendPlay = (req, res) => {
-  axios.put(`${API_PLAYER_URL}/play`, { headers: { authorization: `Bearer ${req.token}` } })
+  axios.put(`${API_PLAYER_URL}/play`, { headers: { authorization: `Bearer ${req.body.token}` } })
     .then((response) => {
       console.log(JSON.stringify(response.data));
     })
@@ -24,7 +25,7 @@ export const sendPlay = (req, res) => {
 };
 
 export const sendPause = (req, res) => {
-  axios.put(`${API_PLAYER_URL}/pause`, { headers: { authorization: `Bearer ${req.token}` } })
+  axios.put(`${API_PLAYER_URL}/pause`, { headers: { authorization: `Bearer ${req.body.token}` } })
     .then((response) => {
       console.log(JSON.stringify(response.data));
     })
