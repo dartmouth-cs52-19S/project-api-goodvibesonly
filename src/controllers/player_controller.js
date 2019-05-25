d00a495cd939c0968aaf96b67c5f6a69fe0fb308
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-05-25 17:05:55 -0400

    test playing a song

--- src/controllers/player_controller.js
+++ src/controllers/player_controller.js
@@ -14,8 +14,12 @@ export const getPlayState = (req, res) => {
 };
 
 export const sendPlay = (req, res) => {
+  const params = {
+    uris: 'spotify:track:4iV5W9uYEdYUVa79Axb7Rh',
+  };
+
   console.log('entered backend play ', req.params.token);
-  axios.put(`${API_PLAYER_URL}/play`, { headers: { authorization: `Bearer ${req.params.token}` } })
+  axios.put(`${API_PLAYER_URL}/play`, { headers: { authorization: `Bearer ${req.params.token}` }, params })
     .then((response) => {
       console.log(JSON.stringify(response.data));
     })
