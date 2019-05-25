a5cdca1953cb4127ba511c94f4c917b4fed35c4c
Angela Li <alidartmouth20@gmail.com>
2019-05-25 18:34:01 -0400

    console loggin

--- src/controllers/playlist_controller.js
+++ src/controllers/playlist_controller.js
@@ -22,6 +22,7 @@ export const getPlaylists = (req, res) => {
 
 export const getPlaylist = (req, res) => {
   Playlist.findById(req.params.id).lean().then((playlist) => {
+    console.log('PLAYLIST RESULT', playlist.result[0]);
     res.json({ playlist: playlist.result[0] });
   })
     .catch((error) => {
