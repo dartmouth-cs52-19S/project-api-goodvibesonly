7194885c53dd3972ca590dbe897738cad0d8f889
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-05-26 18:07:27 -0400

    json

--- src/controllers/player_controller.js
+++ src/controllers/player_controller.js
@@ -7,8 +7,7 @@ const API_PLAYER_URL = 'https://api.spotify.com/v1/me/player';
 export const getPlayState = (req, res) => {
   axios.get(`${API_PLAYER_URL}/currently-playing`, { headers: { authorization: `Bearer ${req.params.token}` } })
     .then((response) => {
-      res.send(response.data);
-      console.log(JSON.stringify(response.data));
+      res.json(response.data);
     })
     .catch((error) => {
       console.log(`spotify api error: ${error}`);
