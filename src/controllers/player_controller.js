acfd9f1b48c73316c35575e6260075bb7b8096b4
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-05-25 22:40:41 -0400

    request play instead

--- src/controllers/player_controller.js
+++ src/controllers/player_controller.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const request = require('request');
+
 const API_PLAYER_URL = 'https://api.spotify.com/v1/me/player';
 
 export const getPlayState = (req, res) => {
@@ -15,13 +17,29 @@ export const getPlayState = (req, res) => {
 
 export const sendPlay = (req, res) => {
   console.log('entered backend play ', req.params.token);
-  axios.put(`${API_PLAYER_URL}/play`, { headers: { authorization: `Bearer ${req.params.token}` } })
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(`spotify api error: ${error}`);
-    });
+  // axios.put(`${API_PLAYER_URL}/play`, { headers: { authorization: `Bearer ${req.params.token}` } })
+  //   .then((response) => {
+  //     console.log(JSON.stringify(response.data));
+  //   })
+  //   .catch((error) => {
+  //     console.log(`spotify api error: ${error}`);
+  //   });
+  const authOptions = {
+    url: 'https://api.spotify.com/v1/me/player',
+    headers: {
+      // eslint-disable-next-line no-buffer-constructor
+      Authorization: `Bearer ${req.params.token}`,
+    },
+    json: true,
+  };
+
+  request.put(authOptions, (error, response, body) => {
+    if (!error) {
+      console.log(response);
+    } else {
+      console.log(error);
+    }
+  });
 };
 
 export const sendPause = (req, res) => {
